780f46c6f5ee2f3af7e23641a5879c3239cf1555
ane <ane@iki.fi>
2011-04-14 22:05:57 +0300

    Added license, improved test case and added some documentation.

--- ArbitraryCheck/Program.cs
+++ ArbitraryCheck/Program.cs
@@ -1,4 +1,33 @@
-﻿using System;
+﻿/*
+ * ArbitraryCheck - A port of QuickCheck to C#
+ * 
+ * Copyright (c) 2011, Antoine Kalmbach <ane@iki.fi>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the author nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,10 +36,17 @@ namespace ArbitraryCheck
 {
     class ArbitraryTest
     {
+        public static List<T> reverse<T>(List<T> collection)
+        {
+            List<T> newColl = collection.ToList(); 
+            newColl.Reverse();
+            return newColl;
+        }
         static void Main(string[] args)
         {
-            ArbitraryCheck.Check<Int32>(input => Math.Abs(input) >= 0);
+            ArbitraryCheck.Check<int>(input => Math.Abs(input) >= 0);
             ArbitraryCheck.Check<List<int>>(inp => inp.Take(5).Count() <= 5);
+            ArbitraryCheck.Check<List<int>>(inp => reverse(reverse(inp)).First() == inp.First());
             Console.Read();
         }
     }
